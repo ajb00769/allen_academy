9bac207fe6a4aeda0286d179b107ec01ce6c4a24
Allen Bercero <abercero@pop-os.localdomain>
2024-04-21 19:09:21 +0800

    added further data modeling

---
+++ api_gateway/gateway/model_utils/year_utils.py
@@ -0,0 +1,13 @@
+from datetime import date
+
+
+def get_current_year() -> int:
+    return date.today().year
+
+
+def get_year_choices() -> list[int]:
+    return [year for year in range(2000, date.today().year + 2)]
+
+
+def get_current_date() -> datetime:
+    return date.today()
--- api_gateway/gateway/models.py
+++ api_gateway/gateway/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from datetime import datetime
 from model_utils.validators import (
     phone_validator,
     student_age_validator,
@@ -12,6 +14,7 @@ from model_utils.choices import (
     EMPLOYEE_TYPE_CHOICES,
     FAMILY_TYPE_CHOICES,
 )
+from model_utils.year_utils import get_current_year, get_year_choices, get_current_date
 
 """
 Keyword Arguments Order:
@@ -329,6 +332,7 @@ class ParentDetail(models.Model):
 
 class Department(models.Model):
     dept_id = models.CharField(
+        primary_key=True,
         max_length=3,
         blank=False,
         null=False,
@@ -352,3 +356,254 @@ class Department(models.Model):
         blank=False,
         null=False,
     )
+
+
+class Salary(models.Model):
+    employee_id = models.ForeignKey(
+        "EmployeeDetail",
+        to_field="account_id",
+        primary_key=True,
+        on_delete=models.PROTECT,
+        blank=False,
+        null=False,
+    )
+    salary = models.FloatField(blank=False, null=False)
+    created_by = models.ForeignKey(
+        "EmployeeDetail",
+        to_field="account_id",
+        primary_key=True,
+        on_delete=models.PROTECT,
+        blank=False,
+        null=False,
+    )
+    create_date = models.DateField(auto_now_add=True)
+    changed_by = models.ForeignKey(
+        "EmployeeDetail",
+        to_field="account_id",
+        blank=False,
+        null=False,
+    )
+    change_date = models.DateField()
+
+
+class EmployeeAttendance(models.Model):
+    employee_id = models.ForeignKey(
+        "EmployeeDetail",
+        to_field="account_id",
+        primary_key=True,
+        on_delete=models.PROTECT,
+        blank=False,
+        null=False,
+    )
+    date_time_in = models.DateTimeField()
+    date_time_out = models.DateTimeField()
+
+
+class EmployeeSalaryHist(models.Model):
+    employee_id = models.ForeignKey(
+        "EmployeeDetail",
+        to_field="account_id",
+        primary_key=True,
+        on_delete=models.PROTECT,
+    )
+    salary_paid = models.FloatField(blank=False, null=False)
+    date_paid = models.DateField()
+    promotion_flag = models.BooleanField(max_length=1, default=False)
+    increase_flag = models.BooleanField(max_length=1, default=False)
+
+
+class StudentViolation(models.Model):
+    student_id = models.ForeignKey(
+        "StudentDetail",
+        to_field="account_id",
+        on_delete=models.PROTECT,
+        blank=False,
+        null=False,
+    )
+    violation_code = models.ForeignKey(
+        "ViolationCode",
+        to_field="violation_code",
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+    )
+
+
+class ViolationCode(models.Model):
+    violation_code = models.CharField(
+        primary_key=True,
+        max_length=3,
+        blank=False,
+        null=False,
+    )
+    violation_desc = models.CharField(
+        max_length=1024,
+        blank=False,
+        null=False,
+    )
+
+
+class Course(models.Model):
+    course_code = models.CharField(
+        primary_key=True,
+        max_length=10,
+        blank=False,
+        null=False,
+    )
+    course_name = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False,
+    )
+    total_units = models.IntegerField()
+    dept_id = models.ForeignKey(
+        "Department",
+        to_field="dept_id",
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+    )
+
+
+class Subject(models.Model):
+    subject_code = models.CharField(
+        primary_key=True,
+        max_length=10,
+        blank=False,
+        null=False,
+    )
+    subject_type = models.CharField(
+        max_length=3,
+        blank=False,
+        null=False,
+    )
+    subject_name = models.CharField(
+        max_length=512,
+        blank=False,
+        null=False,
+    )
+    subject_units = models.IntegerField(blank=False, null=False)
+    weekly_hours = models.DurationField(blank=False, null=False)
+    subject_tuition = models.FloatField(blank=False, null=False)
+    dept_id = models.ForeignKey(
+        "Departments",
+        to_field="dept_id",
+        on_delete=models.PROTECT,
+        blank=False,
+        null=False,
+    )
+
+    @classmethod
+    def get_classes(self):
+        choices = self.objects.all().select_related("classsubject_set")
+        return choices
+
+
+class ClassSubject(models.Model):
+    class_id = models.IntegerField(primary_key=True)
+    subject_code = models.ForeignKey(
+        "Subject",
+        to_field="subject_code",
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+    )
+    subj_block = models.CharField(
+        max_length=10,
+        blank=False,
+        null=False,
+    )
+    professor = models.ForeignKey(
+        "EmployeeDetail",
+        to_field="account_id",
+        on_delete=models.CASCADE,
+        null=False,
+    )
+    semester = models.IntegerField(
+        choices=[1, 2],
+        blank=False,
+        null=False,
+    )
+    school_year = models.IntegerField(
+        choices=get_year_choices(),
+        default=get_current_year(),
+        null=False,
+    )
+    start_date = models.DateField(blank=False, null=False)
+    end_date = models.DateField(blank=False, null=False)
+    completed = models.BooleanField(default=False)
+    active_flag = models.BooleanField(default=True)
+
+    def clean(self):
+        super().clean()
+        if self.start_date >= self.end_date:
+            raise ValidationError("End date should be higher than the start date.")
+        if self.end_date >= get_current_date():
+            raise ValidationError("End date should be higher than the current date.")
+
+    # TODO: Create a scheduled job to run once a day to check the if end_date >= current_date to change the flag
+
+
+class ClassSchedule(models.Model):
+    """
+    Multiple rows for similar classes but different schedules.
+    """
+
+    schedule_id = models.IntegerField(primary_key=True)
+    class_id = models.ForeignKey(
+        "ClassSubject",
+        to_field="class_id",
+        on_delete=models.CASCADE,
+        null=False,
+    )
+    day_of_week = models.IntegerField(choices=[i for i in range(7)])
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    purge_flag = models.BooleanField(default=False)
+
+
+class StudentSubject(models.Model):
+    student_id = models.ForeignKey(
+        "StudentDetail",
+        to_field="account_id",
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+    )
+    subject_code = models.ForeignKey("Subject", to_field="subject_code")
+    year = models.IntegerField(
+        choices=get_year_choices(),
+        default=get_current_year(),
+        null=False,
+    )
+    status = models.CharField(max_length=1)  # pass fail or active
+
+
+class StudentSchedule(models.Model):
+    student_id = models.ForeignKey(
+        "StudentSubject",
+        to_field="student_id",
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+    )
+    schedule_id = models.ForeignKey(
+        "ClassSchedule",
+        to_field("schedule_id"),
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+    )
+
+
+class Grade(models.Model):
+    student_id = models.ForeignKey(
+        "StudentSubject",
+        to_field="student_id",
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+    )
+    subject_id = models.ForeignKey()
+    class_id = models.ForeignKey()
+    grade = models.FloatField()
