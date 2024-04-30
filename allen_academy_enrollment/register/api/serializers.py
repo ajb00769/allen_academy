50f9ca9c1715d85e13d42a502a0b04d5507a1c15
Allen Bercero <abercero@pop-os.localdomain>
2024-05-01 01:55:57 +0800

    initial enrollment feature set up

---
+++ allen_academy_enrollment/register/api/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from register.models import (
+    RegistrationKey,
+    AllAccountId,
+    AllAccount,
+    StudentDetail,
+    EmployeeDetail,
+    ParentDetail,
+)
+
+
+class RegistrationKeySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RegistrationKey
+        fields = "__all__"
+
+
+class AccountIdSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AllAccountId
+        fields = "__all__"
+
+
+class AllAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AllAccount
+        fields = "__all__"
+        extra_kwargs = {"password": {"write_only": True}}
+
+
+class EmployeeDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EmployeeDetail
+        fields = "__all__"
+
+
+class ParentDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ParentDetail
+        fields = "__all__"
